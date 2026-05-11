c9bebe858ad7fe575f527e93621502864f4b0a9e
keerthana_oruganti <keerthanauday012@gmail.com>
2026-05-11 19:06:58 +0530

    Create intro.py with Python basics and example
    Added an introduction to Python with key features and a sample program.

---
+++ intro.py
@@ -0,0 +1,25 @@
+'''
+1. Introduction to Python
+Explanation: Python is a high-level, interpreted programming language known for its simplicity and readability. It supports multiple programming paradigms including procedural, object-oriented, and functional programming.
+
+Key Features:
+
+Dynamically typed
+Garbage collected
+Extensive standard library
+Cross-platform compatibility
+
+Note:
+------
+In python alignment is very important
+You need to follow the spacing correctly to execute the set of statements in an order
+'''
+
+#First Python Program
+
+# This is a comment
+print("Hello, World!")  # Output: Hello, World!
+
+# Getting user input
+name = input("Enter your name: ")
+print(f"Hello, {name}!")  #formatted string
