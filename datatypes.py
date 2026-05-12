79950c70a1f1e0bb99447dbf0c14bda9e15aff2a
keerthana_oruganti <keerthanauday012@gmail.com>
2026-05-12 18:50:38 +0530

    Added datatypes.py file
    Added section header for Numeric Types in datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,28 @@
+'''
+3. Data Types
+Explanation: Python has several built-in data types for representing different kinds of data.
+
+Numeric Types
+--------------
+'''
+# Integers
+age = 25
+binary = 0b1010      # Binary (10 in decimal)
+hexadecimal = 0xFF   # Hexadecimal (255 in decimal)
+
+# Floating-point numbers
+height = 5.9
+scientific = 1.2e-4  # 0.00012
+
+# Complex numbers
+complex_num = 3 + 4j
+print(complex_num.real)  # 3.0
+print(complex_num.imag)  # 4.0
+
+# Boolean
+is_student = True
+is_graduated = False
+print(bool(0))      # False
+print(bool(1))      # True
+print(bool(""))     # False
+print(bool("Hi"))   # True
